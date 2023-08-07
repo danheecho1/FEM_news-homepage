0239f10c5a9fdc264f7a59a5b3ea64e57e441613
Danny Cho <dannycho.dev@gmail.com>
2023-08-06 17:39:47 -0700

    Finished Main component. Also separated global variables in CSS from globals.css to _variables.scss.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { Navbar } from '@/components'
+import { Navbar, Main } from '@/components'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Navbar />
+      <Main />
     </div>
   )
 }
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import frontPageImage from "../public/image-web-3-mobile.jpg";
+import styles from "./Main.module.css";
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+	return (
+		<section className={styles.mainContainer}>
+			<Image
+				src={frontPageImage}
+				alt="Web 3"
+				style={{ maxWidth: "100%", height: "auto" }}
+			/>
+			<div className={styles.mainContainer__textDiv}>
+				<h1 className={styles.mainContainer__textDiv__heading}>The Bright Future of Web 3.0?</h1>
+				<p className={styles.mainContainer__textDiv__paragraph}>
+					We dive into the next evolution of the web that claims to
+					put the power of the platforms back into the hands of the
+					people. But is it really fulfilling its promise?
+				</p>
+				<button className={styles.mainContainer__textDiv__button}>READ MORE</button>
+			</div>
+		</section>
+	);
+};
 
-export default Main
+export default Main;
