1816087bb4b7a845f5ff4d2ad9648603852c295c
Danny Cho <dannycho.dev@gmail.com>
2023-08-06 18:13:38 -0700

    Finished News component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { Navbar, Main } from '@/components'
+import { Navbar, Main, News } from '@/components'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Navbar />
       <Main />
+      <News />
     </div>
   )
 }
--- components/News.tsx
+++ components/News.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import styles from "./News.module.css";
 
 const News = () => {
-  return (
-    <div>News</div>
-  )
-}
+	return (
+		<section className={styles.newsContainer}>
+			<h2 className={styles.newsContainer__heading}>New</h2>
+			<div className={styles.newsContainer__newsItemsDiv}>
+				<div className={styles.newsContainer__newsItemsDiv__individualNewsItemDiv}>
+					<h3 className={styles.newsContainer__newsItemsDiv__individualNewsItemDiv__heading}>Hydrogen VS Electric Cars</h3>
+					<p className={styles.newsContainer__newsItemsDiv__individualNewsItemDiv__paragraph}>Will hydrogen-fueled cars ever catch up to EVs?</p>
+				</div>
+				<hr />
+				<div className={styles.newsContainer__newsItemsDiv__individualNewsItemDiv}>
+					<h3 className={styles.newsContainer__newsItemsDiv__individualNewsItemDiv__heading}>The Downsides of AI Artistry</h3>
+					<p className={styles.newsContainer__newsItemsDiv__individualNewsItemDiv__paragraph}>
+						What are the possible adverse effects of on-demand AI
+						image generation?
+					</p>
+				</div>
+				<hr />
+				<div className={styles.newsContainer__newsItemsDiv__individualNewsItemDiv}>
+					<h3 className={styles.newsContainer__newsItemsDiv__individualNewsItemDiv__heading}>Is VC Funding Drying Up?</h3>
+					<p className={styles.newsContainer__newsItemsDiv__individualNewsItemDiv__paragraph}>
+						Private funding by VC firms is down 50% YOY. We take a
+						look at what that means.
+					</p>
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default News
+export default News;
